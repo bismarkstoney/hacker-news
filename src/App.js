4b6befbd0ed3b97bbb508c72e78ff95ff5fe10b7
bismarkstoney <bismarkstoney@yahoo.com>
2021-03-01 16:53:46 +0800

    added hacker new using only functional and hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [results, setResults] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('react hooks');
+	const [loading, setloading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const searchRef = useRef();
+	useEffect(() => {
+		getResults();
+	}, []);
+
+	const getResults = async () => {
+		setloading(true);
+
+		try {
+			const response = await axios.get(
+				`http://hn.algolia.com/api/v1/search?query=${searchTerm}`
+			);
+			setResults(response.data.hits);
+		} catch (err) {
+			setError(err);
+		}
+		setloading(false);
+	};
+	const handleClearSearch = () => {
+		setSearchTerm('');
+		searchRef.current.focus();
+	};
+	return (
+		<div className='container mx-w-md mx-auto p-4 m-2 bg-purple-lightest shawdow-lg rounded'>
+			<h1 className='text-grey-darkest font-thin'>hacker news</h1>
+
+			<input
+				className='border p-1 rounded'
+				type='text'
+				placeholder='saerch'
+				onChange={(event) => setSearchTerm(event.target.value)}
+				value={searchTerm}
+				ref={searchRef}
+			/>
+
+			<button
+				type='submit'
+				onClick={getResults}
+				className='bg-green-500 rounded m-1 p-1 '>
+				Search
+			</button>
+			<button
+				type='button'
+				onClick={handleClearSearch}
+				className='bg-red-500 rounded m-1 p-1'>
+				clear
+			</button>
+
+			{loading ? (
+				<div>Loading results</div>
+			) : (
+				<ul>
+					{results.map((result) => (
+						<li key={result.objectID}>
+							<a href={result.url}>{result.title}</a>
+						</li>
+					))}
+				</ul>
+			)}
+			{error && <div>{error.message}</div>}
+		</div>
+	);
 }
 
 export default App;
